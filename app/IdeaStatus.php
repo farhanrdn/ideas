368992fd068209c0c7592d8953b07fb37a68da7b
Farhan Ramadhan <farhanramadhan123@gmail.com>
2026-02-21 14:16:59 +0700

    menambahkan fungsi step

---
+++ app/IdeaStatus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+enum IdeaStatus: string
+{
+    case PENDING = 'pending';
+    case IN_PROGRESS = 'in_progress';
+    case COMPLETE = 'complete';
+
+    function label(): string
+    {
+        return match ($this) {
+            self::PENDING => 'Pending',
+            self::IN_PROGRESS => 'In Progress',
+            self::COMPLETE => 'Complete',
+        };
+    }
+};
